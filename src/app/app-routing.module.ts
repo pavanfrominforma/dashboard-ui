3937e7485b00ffbbbeb3ce39b535468d4677d5e8
Pavan Kumar Yekabote <yekabote.kumar@meritgroup.co.uk>
2022-10-26 00:23:46 +0530

    Feat: Added routes and their pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { FeedsComponent } from './subcomps/feeds/feeds.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+      path:"", component: MainPageComponent, 
+      children: [
+        { path: "vdp", children: [
+           { path: "feeds", component:  FeedsComponent }
+        ]}
+      ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
