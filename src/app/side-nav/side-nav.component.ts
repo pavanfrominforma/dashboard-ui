4023d0e3750707a6cb580dbe110d463ce5df828c
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-26 14:05:31 +0530

    Style: Fix formatting

--- src/app/side-nav/side-nav.component.ts
+++ src/app/side-nav/side-nav.component.ts
@@ -4,53 +4,45 @@ import * as bootstrap from 'bootstrap';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.scss']
+  styleUrls: ['./side-nav.component.scss'],
 })
 export class SideNavComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.initialize();
   }
 
-  initialize(){
-    document.addEventListener("DOMContentLoaded", function () {
+  initialize() {
+    document.addEventListener('DOMContentLoaded', function () {
+      document
+        .querySelectorAll('#sidebar .nav-link')
+        .forEach(function (element) {
+          element.addEventListener('click', function (e) {
+            let nextEl = element.nextElementSibling as any;
+            let parentEl = element.parentElement as any;
 
-      document.querySelectorAll('#sidebar .nav-link').forEach(function (element) {
-    
-        element.addEventListener('click', function (e) {
+            if (nextEl && parentEl.classList.contains('has-submenu')) {
+              e.preventDefault();
 
-          
-    
-          let nextEl = element.nextElementSibling as any;
-          let parentEl = element.parentElement as any;
+              if (nextEl.classList.contains('show')) {
+                let mycollapse = new bootstrap.Collapse(nextEl);
+                mycollapse.hide();
+              } else {
+                let mycollapse = new bootstrap.Collapse(nextEl);
 
-
-
-          if (nextEl && parentEl.classList.contains('has-submenu')) {
-            e.preventDefault();
-    
-            if (nextEl.classList.contains('show')) {
-              let mycollapse = new bootstrap.Collapse(nextEl);
-              mycollapse.hide();
-            } else {
-
-              let mycollapse = new bootstrap.Collapse(nextEl);
-
-              mycollapse.show();
-              // find other submenus with class=show
-              var opened_submenu = parentEl.parentElement.querySelector('.submenu.show');
-              // if it exists, then close all of them
-              if (opened_submenu) {
-                new bootstrap.Collapse(opened_submenu);
+                mycollapse.show();
+                // find other submenus with class=show
+                var opened_submenu =
+                  parentEl.parentElement.querySelector('.submenu.show');
+                // if it exists, then close all of them
+                if (opened_submenu) {
+                  new bootstrap.Collapse(opened_submenu);
+                }
               }
             }
-          }
-        }); // addEventListener
-      }) // forEach
-    }); 
+          }); // addEventListener
+        }); // forEach
+    });
   }
 }
-
-
