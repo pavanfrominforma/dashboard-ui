385bc26dd592fd5d7627deb1a3cabb26e03ccfbe
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-26 14:04:14 +0530

    Feat: Added api service, to make api calls

---
+++ src/app/services/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  apiUrl = environment.apiUrl;
+  headers = { 'Content-type': 'application/json' };
+
+  constructor(private http: HttpClient) {}
+
+  getVdpFeeds() {
+    const url = `${this.apiUrl}/vdp/feeds`;
+    return this.http.get(url, { headers: this.headers });
+  }
+}
