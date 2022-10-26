0d5301c6e6c8f0e87fa39dfa72659142a2a93491
Pavan Kumar Yekabote <kumar.pavan@informa.com>
2022-10-26 14:06:23 +0530

    Feat: Added deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
@@ -14,13 +14,10 @@ import { FeedsComponent } from './subcomps/feeds/feeds.component';
     TopNavComponent,
     SideNavComponent,
     MainPageComponent,
-    FeedsComponent
+    FeedsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
