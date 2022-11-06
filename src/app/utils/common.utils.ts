eadae67e151e17635438e4af6f9544c4c484ffe9
Pavan Kumar Yekabote <yekabote.kumar@meritgroup.co.uk>
2022-11-06 22:54:09 +0530

    Feat: Added sorter methods

--- src/app/utils/common.utils.ts
+++ src/app/utils/common.utils.ts
@@ -1,4 +1,39 @@
+import * as moment from "moment";
+
 export const prefixZero = (digit: number) => {
     const digitstr = String(digit);
     return digitstr.length > 1 ? digit : "0" + digitstr;
+};
+
+export const stringSorter = (
+    prev: any,
+    next: any,
+    sortField: string = null
+) => {
+    const prevValue: string = sortField != null ? prev[sortField] : prev;
+    const nextValue: string = sortField != null ? next[sortField] : next;
+    return prevValue.localeCompare(nextValue);
+};
+
+export const numericSorter = (
+    prev: any,
+    next: any,
+    sortField: string = null
+) => {
+    const prevValue: number = sortField != null ? prev[sortField] : prev;
+    const nextValue: number = sortField != null ? next[sortField] : next;
+    return prevValue - nextValue;
+};
+
+export const dateSorter = (prev: any, next: any, sortField: string = null) => {
+    const prevValue: Date = sortField != null ? prev[sortField] : prev;
+    const nextValue: Date = sortField != null ? next[sortField] : next;
+    return moment(prevValue).diff(nextValue, "ms");
+};
+
+export const getSorterBy = (datatype: string) => {
+    if (datatype == "number") return numericSorter;
+    if (datatype == "string") return stringSorter;
+    if (datatype == "date") return dateSorter;
+    return null;
 };
